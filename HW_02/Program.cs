883c864d0e269a6de1d7276c2f85c6cf0c68d64e
YaOn999 <lumous999@gmail.com>
2022-11-30 21:32:04 +0300

    New HW_03 and

--- HW_02/Program.cs
+++ HW_02/Program.cs
@@ -6,9 +6,15 @@ int FirstNumber = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int SecondNumber = int.Parse(Console.ReadLine());
 
-if (SecondNumber < FirstNumber) {
-    Console.WriteLine($"max = {FirstNumber}");
+Console.Write("Введите третье число: ");
+int ThitdNumber = int.Parse(Console.ReadLine());
+
+int max = FirstNumber;
+
+if (max < SecondNumber) {
+    max = SecondNumber;
     }
-    else {
-    Console.WriteLine($"max = {SecondNumber}");
+if (max < ThitdNumber) {
+    max = ThitdNumber;
     }
+Console.WriteLine($"Максимальное число {max}");
---
+++ HW_03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+
+if (Number % 2 == 0) {
+     Console.WriteLine("число четное");
+     }
+     else {
+     Console.WriteLine("число нечетное");
+     }
