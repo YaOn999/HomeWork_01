6a48cafd850b24d36d5b962251ec77a5452de316
YaOn999 <lumous999@gmail.com>
2022-11-30 21:26:31 +0300

    New HM_02

---
+++ HW_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int FirstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int SecondNumber = int.Parse(Console.ReadLine());
+
+if (SecondNumber < FirstNumber) {
+    Console.WriteLine($"max = {FirstNumber}");
+    }
+    else {
+    Console.WriteLine($"max = {SecondNumber}");
+    }
